1a5f25b7c444d5ae2aa7b8095db9f6e4167cfc1d
Sebastian Lauwers <sl@mailicio.us>
2024-11-06 20:02:05 +0100

    Implement 2023-13

---
+++ 2023/13/src/lib.rs
@@ -0,0 +1,119 @@
+use common::Answer;
+
+#[derive(Debug)]
+struct Pattern<'a> {
+    rows: Vec<&'a str>,
+}
+
+impl<'a> From<&'a str> for Pattern<'a> {
+    fn from(s: &'a str) -> Self {
+        let rows = s.lines().collect();
+
+        Pattern { rows }
+    }
+}
+
+impl Pattern<'_> {
+    fn horizontal_numbers(&self) -> Vec<u32> {
+        self.rows
+            .iter()
+            .map(|row| {
+                row.chars()
+                    .enumerate()
+                    .filter_map(|(i, c)| if c == '#' { Some(1 << i) } else { None })
+                    .sum()
+            })
+            .collect()
+    }
+
+    fn vertical_numbers(&self) -> Vec<u32> {
+        (0..self.rows[0].len())
+            .map(|i| {
+                self.rows
+                    .iter()
+                    .enumerate()
+                    .map(|(j, row)| (j, row.chars().nth(i)))
+                    .filter_map(|(j, c)| if c == Some('#') { Some(1 << j) } else { None })
+                    .sum()
+            })
+            .collect()
+    }
+
+    fn summarize(&self) -> usize {
+        let (v, _) = find_reflection(&self.vertical_numbers());
+        let (h, _) = find_reflection(&self.horizontal_numbers());
+
+        v + h * 100
+    }
+
+    fn summarize_smudges(&self) -> usize {
+        let (_, v) = find_reflection(&self.vertical_numbers());
+        let (_, h) = find_reflection(&self.horizontal_numbers());
+
+        v + h * 100
+    }
+}
+
+#[inline]
+fn bit_diff(a: u32, b: u32) -> u32 {
+    (a ^ b).count_ones()
+}
+
+fn find_reflection(values: &[u32]) -> (usize, usize) {
+    values
+        .windows(2)
+        .enumerate()
+        .filter_map(|(i, w)| {
+            if bit_diff(w[0], w[1]) <= 1 {
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .fold((0, 0), |acc, pos| {
+            let mut min = pos;
+            let mut max = pos + 1;
+            let mut diff = 0;
+
+            loop {
+                diff += bit_diff(values[min], values[max]);
+
+                if diff > 1 {
+                    return acc;
+                }
+
+                if min > 0 && max < values.len() - 1 {
+                    min -= 1;
+                    max += 1;
+                } else {
+                    break;
+                }
+            }
+
+            match diff {
+                0 => (acc.0 + pos + 1, acc.1),
+                1 => (acc.0, acc.1 + pos + 1),
+                _ => acc,
+            }
+        })
+}
+
+pub fn step1(s: &str) -> Answer {
+    let patterns: Vec<_> = s.split("\n\n").map(Pattern::from).collect();
+
+    patterns
+        .iter()
+        .map(Pattern::summarize)
+        .sum::<usize>()
+        .into()
+}
+
+pub fn step2(s: &str) -> Answer {
+    let patterns: Vec<_> = s.split("\n\n").map(Pattern::from).collect();
+
+    patterns
+        .iter()
+        .map(Pattern::summarize_smudges)
+        .sum::<usize>()
+        .into()
+}
