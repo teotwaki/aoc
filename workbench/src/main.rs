be7973e076b76670e19dc4b12406f7e824499c62
Sebastian Lauwers <sl@mailicio.us>
2024-11-07 11:14:22 +0100

    Implement 2023-16

--- workbench/src/main.rs
+++ workbench/src/main.rs
@@ -31,7 +31,7 @@ fn main() -> Result<()> {
         };
     }
 
-    add!(2023, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15);
+    add!(2023, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16);
 
     let cli = Cli::parse();
     let year = cli.year.unwrap_or_else(|| registry.latest_year());
