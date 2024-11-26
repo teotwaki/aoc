88b8b76d43cf4531cdd4a4cee0c0046966d84b51
Sebastian Lauwers <sl@mailicio.us>
2024-11-26 09:37:50 +0100

    Make day template easier to use and more complete

--- day_template/src/lib.rs
+++ day_template/src/lib.rs
@@ -4,6 +4,18 @@ pub fn step1(s: &str) -> Answer {
     unimplemented!()
 }
 
-pub fn step2(s: &str) -> Answer {
-    unimplemented!()
+pub fn step2(_: &str) -> Answer {
+    ().into()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT: &str = r#""#;
+
+    #[test]
+    fn test_reality() {
+        assert_eq!(INPUT, "");
+    }
 }
