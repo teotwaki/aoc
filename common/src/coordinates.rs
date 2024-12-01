d6c300f513a73a90caffa447d6b9d6f03841081e
Sebastian Lauwers <sl@mailicio.us>
2024-12-01 13:06:44 +0100

    Add couple more convenience functions to Coordinates

--- common/src/coordinates.rs
+++ common/src/coordinates.rs
@@ -1,4 +1,4 @@
-use num_traits::{NumCast, PrimInt, ToPrimitive};
+use num_traits::{Float, NumCast, PrimInt, ToPrimitive};
 use std::ops::{AddAssign, SubAssign};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -59,12 +59,37 @@ where
     }
 }
 
-impl<T, U> From<(U, U)> for Coordinates<T>
+impl<T, U, V> From<(U, V)> for Coordinates<T>
 where
     T: NumCast,
     U: ToPrimitive,
+    V: ToPrimitive,
 {
-    fn from(value: (U, U)) -> Self {
+    fn from(value: (U, V)) -> Self {
         Self::new(T::from(value.0).unwrap(), T::from(value.1).unwrap())
+    }
+}
+
+impl<T> Coordinates<T>
+where
+    T: Copy + PrimInt,
+{
+    pub fn to_float<F: Float>(&self) -> Coordinates<F> {
+        let x = F::from(self.x).unwrap();
+        let y = F::from(self.y).unwrap();
+
+        Coordinates::new(x, y)
+    }
+}
+
+impl<F> Coordinates<F>
+where
+    F: Copy + Float,
+{
+    pub fn to_int<T: PrimInt>(&self) -> Coordinates<T> {
+        let x = T::from(self.x).unwrap();
+        let y = T::from(self.y).unwrap();
+
+        Coordinates::new(x, y)
     }
 }
