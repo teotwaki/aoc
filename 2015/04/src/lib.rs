720ff443dd615b4e39bd40324eca6be6238b9595
Sebastian Lauwers <sl@mailicio.us>
2024-12-04 22:42:20 +0100

    Implement 2015 day 4

---
+++ 2015/04/src/lib.rs
@@ -0,0 +1,34 @@
+use common::Answer;
+
+fn find_md5_prefix(secret: &str, prefix: &str) -> Answer {
+    for i in 0..100_000_000 {
+        let attempt = format!("{}{i}", secret.trim_end());
+        let digest = md5::compute(attempt.as_bytes());
+
+        if format!("{:x}", digest).starts_with(prefix) {
+            return i.into();
+        }
+    }
+
+    ().into()
+}
+
+pub fn step1(s: &str) -> Answer {
+    find_md5_prefix(s, "00000")
+}
+
+pub fn step2(s: &str) -> Answer {
+    find_md5_prefix(s, "000000")
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT: &str = r#""#;
+
+    #[test]
+    fn parse_extracts_correct_number_of_lines() {
+        assert_eq!(parse(INPUT).len(), 10);
+    }
+}
