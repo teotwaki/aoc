15098c269a8fff769d94213e5435dd4a47006e2c
Sebastian Lauwers <sl@mailicio.us>
2024-12-05 15:25:43 +0100

    Implement 2015 day 8

---
+++ 2015/08/src/lib.rs
@@ -0,0 +1,46 @@
+use common::Answer;
+
+pub fn step1(s: &str) -> Answer {
+    let apostrophes = s.lines().count() * 2;
+    let s = s.replace("\n", "");
+    let code_length = s.len();
+    let mut s = s.replace(r"\\", "B").replace(r#"\""#, "A");
+
+    while let Some(pos) = s.find(r"\x") {
+        s.replace_range(pos..(pos + 4), "X");
+    }
+
+    let data_length = s.len() - apostrophes;
+
+    (code_length - data_length).into()
+}
+
+pub fn step2(s: &str) -> Answer {
+    let apostrophes = s.lines().count() * 2;
+    let s = s.replace("\n", "");
+    let code_length = s.len();
+
+    let s = s.replace(r"\", r"\\").replace(r#"""#, r#"\""#);
+
+    (s.len() + apostrophes - code_length).into()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT: &str = r#"""
+"abc"
+"aaa\"aaa"
+"\x27""#;
+
+    #[test]
+    fn step1_finds_correct_answer() {
+        assert_eq!(step1(INPUT), Answer::Unsigned(12));
+    }
+
+    #[test]
+    fn step2_finds_correct_answer() {
+        assert_eq!(step2(INPUT), Answer::Unsigned(19));
+    }
+}
