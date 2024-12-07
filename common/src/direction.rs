582f2a08ff9f496611aa4603f0763a4d3e8367d5
Sebastian Lauwers <sl@mailicio.us>
2024-12-07 14:02:30 +0100

    Give directions the ability to turn

--- common/src/direction.rs
+++ common/src/direction.rs
@@ -6,3 +6,41 @@ pub enum Direction {
     Down,
     Left,
 }
+
+impl Direction {
+    #[inline]
+    #[must_use]
+    pub fn clockwise(&self) -> Self {
+        use Direction::*;
+
+        match self {
+            Up => Right,
+            Right => Down,
+            Down => Left,
+            Left => Up,
+        }
+    }
+
+    #[inline]
+    pub fn turn_clockwise(&mut self) {
+        *self = self.clockwise();
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn counterclockwise(&self) -> Self {
+        use Direction::*;
+
+        match self {
+            Up => Left,
+            Left => Down,
+            Down => Right,
+            Right => Up,
+        }
+    }
+
+    #[inline]
+    pub fn turn_counterclockwise(&mut self) {
+        *self = self.counterclockwise();
+    }
+}
