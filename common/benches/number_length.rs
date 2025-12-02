7cc081bd7e8235a058c26de5f5fae0bde6cd5e6e
Sebastian Lauwers <sl@lauwe.rs>
2025-12-02 13:12:09 +0100

    chore: clippy and rustfmt

--- common/benches/number_length.rs
+++ common/benches/number_length.rs
@@ -1,5 +1,5 @@
 use common::utils::{number_length, number_length_successors};
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, criterion_group, criterion_main};
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("number_length", |b| b.iter(|| number_length(u64::MAX)));
