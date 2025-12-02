461950b2a36f2cacb1542f0713964899c3065901
Sebastian Lauwers <sl@lauwe.rs>
2025-12-02 13:00:48 +0100

    feat: Implement 2025-02 part 2

--- 2025/02/src/lib.rs
+++ 2025/02/src/lib.rs
@@ -1,4 +1,7 @@
-use common::{Answer, utils::number_length};
+use common::{
+    Answer,
+    utils::{factors, number_length},
+};
 
 type IntType = u64;
 
@@ -50,8 +53,38 @@ pub fn step1(s: &str) -> Answer {
     )
 }
 
-pub fn step2(_: &str) -> Answer {
-    ().into()
+fn split_digits(n: IntType, parts: usize) -> Vec<IntType> {
+    let factor = 10u64.pow((number_length(n) / parts) as u32);
+    let mut result = Vec::new();
+    let mut current = n;
+
+    for _ in 0..parts {
+        let digit = current % factor;
+        result.push(digit);
+        current /= factor;
+    }
+
+    result
+}
+
+fn is_valid_complex_id(id: IntType) -> bool {
+    let length = number_length(id);
+    let factors = factors(length as u64);
+
+    factors.iter().skip(1).all(|&factor| {
+        let segments = split_digits(id, factor as usize);
+        segments.iter().skip(1).any(|&v| v != segments[0])
+    })
+}
+
+pub fn step2(s: &str) -> Answer {
+    Answer::Unsigned(
+        parse(s)
+            .iter()
+            .flat_map(|r| r.0..=r.1)
+            .filter(|id| !is_valid_complex_id(*id))
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -71,6 +104,11 @@ mod test_2025_02 {
     }
 
     #[test]
+    fn step2_correctly_counts_invalid_ids() {
+        assert_eq!(step2(INPUT), Answer::Unsigned(4174379265));
+    }
+
+    #[test]
     fn is_valid_id_identifies_correct_values() {
         assert!(is_valid_id(123));
         assert!(is_valid_id(124));
@@ -83,5 +121,29 @@ mod test_2025_02 {
         assert!(!is_valid_id(1212));
         assert!(!is_valid_id(22));
         assert!(!is_valid_id(123123));
+    }
+
+    #[test]
+    fn is_valid_complex_id_identifies_correct_values() {
+        assert!(is_valid_complex_id(12));
+        assert!(is_valid_complex_id(13));
+        assert!(is_valid_complex_id(998));
+        assert!(is_valid_complex_id(998));
+        assert!(is_valid_complex_id(2121212118));
+    }
+
+    #[test]
+    fn is_valid_complex_id_identifies_incorrect_values() {
+        assert!(!is_valid_complex_id(11));
+        assert!(!is_valid_complex_id(22));
+        assert!(!is_valid_complex_id(999));
+        assert!(!is_valid_complex_id(1010));
+        assert!(!is_valid_complex_id(1188511885));
+        assert!(!is_valid_complex_id(222222));
+        assert!(!is_valid_complex_id(446446));
+        assert!(!is_valid_complex_id(38593859));
+        assert!(!is_valid_complex_id(565656));
+        assert!(!is_valid_complex_id(824824824));
+        assert!(!is_valid_complex_id(2121212121));
     }
 }
