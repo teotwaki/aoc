777e5d77b42bc0c3e1125de02fc8090981cf1f4f
Sebastian Lauwers <sl@lauwe.rs>
2025-12-10 23:12:35 +0100

    feat: Fix logic and implement part 1

--- 2025/10/src/lib.rs
+++ 2025/10/src/lib.rs
@@ -1,19 +1,18 @@
-use common::{Answer, utils::number_length_binary};
+use common::Answer;
 use itertools::Itertools;
 
 type IntType = u16;
 
-fn parse_target(s: &str) -> IntType {
+fn parse_target(s: &str) -> (IntType, usize) {
     let s = &s[1..s.len() - 1];
     let mut val = 0;
 
     s.chars()
-        .rev()
         .enumerate()
         .filter(|(_, c)| *c == '#')
         .for_each(|(i, _)| val += 1 << i);
 
-    val
+    (val, s.len())
 }
 
 fn parse_button(s: &str, target_length: usize) -> IntType {
@@ -22,18 +21,18 @@ fn parse_button(s: &str, target_length: usize) -> IntType {
 
     s.split(',')
         .map(|c| c.parse::<usize>().unwrap())
-        .for_each(|i| val += 1 << target_length.saturating_sub(i));
+        .for_each(|i| val += 1 << i);
 
-    val
+    val & ((1 << target_length) - 1)
 }
 
 fn parse_line(s: &str) -> (IntType, Vec<IntType>) {
     let parts = s.split_whitespace().collect::<Vec<_>>();
 
-    let target = parse_target(parts[0]);
+    let (target, lights_count) = parse_target(parts[0]);
     let buttons = parts[1..parts.len() - 1]
         .iter()
-        .map(|s| parse_button(s, number_length_binary(target.into())))
+        .map(|s| parse_button(s, lights_count))
         .collect();
 
     (target, buttons)
@@ -52,7 +51,7 @@ pub fn step1(s: &str) -> Answer {
             buttons
                 .iter()
                 .powerset()
-                .find(|set| set.iter().fold(u16::MAX, |a, b| a ^ **b) == *target)
+                .find(|set| set.iter().fold(0, |a, b| a ^ **b) == *target)
                 .map(|set| set.len())
                 .unwrap()
         })
@@ -67,15 +66,20 @@ pub fn step2(_: &str) -> Answer {
 #[cfg(test)]
 mod test {
     use super::*;
+    use parameterized::parameterized;
 
     #[test]
     fn parse_target_finds_correct_value() {
-        assert_eq!(parse_target("[.##.]"), 6);
+        assert_eq!(parse_target("[.##.]"), (6, 4));
     }
 
-    #[test]
-    fn parse_button_finds_correct_value() {
-        assert_eq!(parse_button("(1,2)", 3), 6);
+    #[parameterized(
+        input1 = { "(1,2)", "(0,1,2,3,4)", "(4)" },
+        input2 = { 3, 5, 1 },
+        result = { 6, 31, 0 },
+    )]
+    fn parse_button_finds_correct_value(input1: &str, input2: usize, result: IntType) {
+        assert_eq!(parse_button(input1, input2), result);
     }
 
     const INPUT: &str = r#"[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
@@ -87,12 +91,10 @@ mod test {
         assert_eq!(parse(INPUT).len(), 3);
     }
 
-    /*
     #[test]
     fn step1_computes_expected_sample_result() {
         assert_eq!(step1(INPUT), Answer::Unsigned(7));
     }
-    */
 
     /*
     #[test]
